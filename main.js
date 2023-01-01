8d145ee92cb16a032fdb4c6cbd3054fb7e9d9d1e
jebomin <42407430+jebomin@users.noreply.github.com>
2023-01-01 19:44:57 +0900

    JS_navbar menu toggle&project UI

--- main.js
+++ main.js
@@ -19,7 +19,14 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
